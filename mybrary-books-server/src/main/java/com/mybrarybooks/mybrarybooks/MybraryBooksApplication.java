5e7ca48eccfce73ffdd9efd9cc63ca5000a01b8a
rebaderbs <rderbak@gmail.com>
2023-03-06 12:04:04 -0600

    added React frontend files and Spring Boot backend files

---
+++ mybrary-books-server/src/main/java/com/mybrarybooks/mybrarybooks/MybraryBooksApplication.java
@@ -0,0 +1,13 @@
+package com.mybrarybooks.mybrarybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybraryBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybraryBooksApplication.class, args);
+	}
+
+}
---
+++ mybrary-books-server/src/test/java/com/mybrarybooks/mybrarybooks/MybraryBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybrarybooks.mybrarybooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybraryBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
